c8a4729cce5daeaa2c9f01f3832437faadc54311
Xuan <108101A1@gm.chihlee.edu.tw>
2023-12-14 14:13:03 +0800

    Login/LoginHeader.js更改下拉式選單顯示的文字

--- src/Component/Login/LoginHeader.js
+++ src/Component/Login/LoginHeader.js
@@ -7,17 +7,6 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const LoginHeader = () => {
 
-    const [dropdownTitle, setDropdownTitle] = useState("購物網-前台");
-
-    const handleDropdownSelect = (eventKey) => {
-        // 根據選擇的 eventKey 更新標題文字
-        if (eventKey === "consumer") {
-            setDropdownTitle("購物網-前台");
-        } else if (eventKey === "admin") {
-            setDropdownTitle("購物網-後台");
-        }
-    };
-
     return (
         <Navbar sticky="top" bg="dark" variant={"dark"} expand="lg">
             <Navbar.Brand as={Link} to="/">E-Commerce</Navbar.Brand>
@@ -25,9 +14,9 @@ const LoginHeader = () => {
             <Navbar.Collapse id="navbarScroll">
                 <Nav className="mr-auto">
                     {/* to 路徑對應 Route path */}
-                    <NavDropdown title={dropdownTitle} id="collasible-nav-dropdown" onSelect={handleDropdownSelect}>
-                        <NavDropdown.Item as={Link} to="/FrontEndLoginPage" eventKey="consumer">購物網-前台</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/BackEndLoginPage" eventKey="admin">購物網-後台</NavDropdown.Item>
+                    <NavDropdown title="登入模式" id="collasible-nav-dropdown">
+                        <NavDropdown.Item as={Link} to="/FrontEndLoginPage">購物網-前台</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="/BackEndLoginPage">購物網-後台</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
